8ad28d3139614e1d07bd0fc1ea1fcde243287754
jasantor <81903968+jasantor@users.noreply.github.com>
2021-04-04 15:40:52 +0200

    Add files via upload

---
+++ Metodos.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package barajaespañola;
+import java.util.Random;
+/**
+ *
+ * @author Usuario
+ */
+class Metodos {
+    public static int generaNumeroEnteroAleatorio(int minimo, int maximo) {
+        int num = (int) (Math.random() * (minimo - (maximo + 1)) + (maximo + 1));
+        return num;
+    }
+}
